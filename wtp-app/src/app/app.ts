ec13a8f4fd4d86ced5f4c4e84217ec8bfdbae5c9
Paula Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-04-10 20:07:01 -0600

    Changes in routes in app.ts

--- wtp-app/src/app/app.ts
+++ wtp-app/src/app/app.ts
@@ -1,9 +1,31 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../components/navbar/navbar';
+import { Hero } from '../components/hero/hero';
+import { ComoFunciona } from '../components/como-funciona/como-funciona';
+import { Problema } from '../components/problema/problema';
+import { Mercado } from '../components/mercado/mercado';
+import { ModeloNegocio } from '../components/modelo-negocio/modelo-negocio';
+import { Tecnologias } from '../components/tecnologias/tecnologias';
+import { Escalabilidad } from '../components/escalabilidad/escalabilidad';
+import { Equipo } from '../components/equipo/equipo';
+import { Footer } from '../components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Navbar,
+    Hero,
+    ComoFunciona,
+    Problema,
+    Mercado,
+    ModeloNegocio,
+    Tecnologias,
+    Escalabilidad,
+    Equipo,
+    Footer,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
