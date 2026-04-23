b6ddfd0088bf94e7b3b72640e60a5bf6f310206f
Paula Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-04-23 08:47:08 -0600

    Components in angular created

---
+++ Final_project/Frontend/final-wtp/src/components/offers-section/offers-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offers-section',
+  imports: [],
+  templateUrl: './offers-section.html',
+  styleUrl: './offers-section.css',
+})
+export class OffersSection {
+
+}
