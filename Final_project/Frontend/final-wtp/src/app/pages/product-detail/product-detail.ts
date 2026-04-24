a54f7455aaf1fe29b8fc51d1f3a49b3db37fc3a7
Paula Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-04-23 18:27:51 -0600

    Basic navbar

--- Final_project/Frontend/final-wtp/src/app/pages/product-detail/product-detail.ts
+++ Final_project/Frontend/final-wtp/src/app/pages/product-detail/product-detail.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { SearchNavbar } from '../../../components/search-navbar/search-navbar';
+import { ProductDetail as ProductDetailComponent } from '../../../components/product-detail/product-detail';
+import { ProductSpecs } from '../../../components/product-specs/product-specs';
+import { ShoppingSection } from '../../../components/shopping-section/shopping-section';
+import { SimilarProducts } from '../../../components/similar-products/similar-products';
 
 @Component({
   standalone: true,
   selector: 'app-product-detail',
-  imports: [],
+  imports: [SearchNavbar, ProductDetailComponent, ProductSpecs, ShoppingSection, SimilarProducts],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css',
 })
