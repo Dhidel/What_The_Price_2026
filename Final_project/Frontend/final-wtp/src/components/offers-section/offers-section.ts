ecfc7809f6cef3d7d4dcda10cbef3a077c91dbc4
DhidelOsorio <djosorio@ufm.edu>
2026-04-24 13:26:33 -0600

    Principal-page Offers
    The principal page offers was added

--- Final_project/Frontend/final-wtp/src/components/offers-section/offers-section.ts
+++ Final_project/Frontend/final-wtp/src/components/offers-section/offers-section.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-offers-section',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './offers-section.html',
   styleUrl: './offers-section.css',
 })
-export class OffersSection {
+export class OffersSection implements OnInit {
 
+  currentIndex = 0;
+
+  banners = [
+    {
+      img: 'images/iphone.png',
+      title: 'Iphone 17 pro max',
+      subtitle: 'El mejor teléfono del año',
+      descripcion: 'Oferta de verano'
+    },
+    {
+      img: 'images/macbook.png',
+      title: 'Macbook Pro',
+      subtitle: 'Potencia sin límites',
+      descripcion: 'Descuento exclusivo'
+    },
+    {
+      img: 'images/airfryer.png',
+      title: 'Air Fryer',
+      subtitle: 'Tecnología para tu hogar',
+      descripcion: 'Ahorra energía'
+    },
+    {
+      img: 'images/televisor.png',
+      title: 'Smart TV 4K',
+      subtitle: 'Cine en casa',
+      descripcion: 'Calidad ultra HD'
+    }
+  ];
+
+// Para que se vaya moviendo cada 4 segundos (aún tengo dudas de como implementarlo)
+  ngOnInit() {
+    setInterval(() => {
+      this.next();
+    }, 4000);
+  }
+
+// Actualiza el indice en donde cada oferta está
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.banners.length;
+  }
+
+// Ayuda a los botones a saber a donde deben ir
+  goTo(index: number) {
+    this.currentIndex = index;
+  }
 }
