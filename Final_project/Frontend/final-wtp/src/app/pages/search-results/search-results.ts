9132944cb20d90de8ee7333e33b7e1bfe223bb67
Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-05-02 18:43:06 -0600

    Revert "PR Ticket2 mongo db"

--- Final_project/Frontend/final-wtp/src/app/pages/search-results/search-results.ts
+++ Final_project/Frontend/final-wtp/src/app/pages/search-results/search-results.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { SearchNavbar } from '../../../components/search-navbar/search-navbar';
 
 @Component({
   selector: 'app-search-results',
-  imports: [SearchNavbar],
+  imports: [],
   templateUrl: './search-results.html',
   styleUrl: './search-results.css',
 })
--- Final_project/Frontend/final-wtp/src/components/search-bar/search-bar.ts
+++ Final_project/Frontend/final-wtp/src/components/search-bar/search-bar.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,9 +7,5 @@ import { Router } from '@angular/router';
   styleUrl: './search-bar.css',
 })
 export class SearchBar {
-  constructor(private router: Router) {}
 
-  goToSearch() {
-    this.router.navigate(['/search']);
-  }
 }
